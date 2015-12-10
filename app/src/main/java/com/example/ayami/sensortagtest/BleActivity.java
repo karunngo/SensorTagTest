d4ad3acc16a637511f30c34e71849ce5ffe54ec5
karunngo <karu@ht.sfc.keio.ac.jp>
2015-12-10 22:14:04 +0900

    おおかたのエラー解消

--- app/src/main/java/com/example/ayami/sensortagtest/BleActivity.java
+++ app/src/main/java/com/example/ayami/sensortagtest/BleActivity.java
@@ -31,8 +31,7 @@ import android.widget.TextView;
 
 import java.util.UUID;
 
-public class BleActivity extends AppCompatActivity implements
-BluetoothAdapter.LeScanCallback{
+public class BleActivity extends AppCompatActivity implements BluetoothAdapter.LeScanCallback{
     private static final long SCAN_PERIOD =10000; // BLEスキャンのタイムアウト(ミリ秒)
     private static final String DEVICE_NAME = "SensorTag 2.0";//機器の名前
     private static final String DEVICE_ACCELEROMATER_SERVICE_UUID ="F000AA10-0451-4000-B000-000000000000 ";
@@ -47,7 +46,8 @@ BluetoothAdapter.LeScanCallback{
     private BluetoothManager mBluetoothManager;
     private BluetoothGatt mBluetoothGatt;
     private TextView mStatusText;
-    //このテキストは☆をあらわしてる
+
+        //このテキストは☆をあらわしてる
 
 
     @Override
@@ -133,86 +133,94 @@ BluetoothAdapter.LeScanCallback{
 
         }
     }
-    private final BluetoothGattCallback mBluetoothGattCallback = new BluetoothGattCallback(){
-    //connectGatt()が成功するとこいつ↓が自動で呼ばれる。ここでサービスを検出
-    @Override
-    public void onConnectionStateChange(BluetoothGatt gatt,int status,int newState){
-        Log.d(TAG, "onConnectionStateChange:" + status + "->" + newState);
+    private final BluetoothGattCallback mBluetoothGattCallback = new BluetoothGattCallback() {
+        //connectGatt()が成功するとこいつ↓が自動で呼ばれる。ここでサービスを検出
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            Log.d(TAG, "onConnectionStateChange:" + status + "->" + newState);
 
-        if(newState== BluetoothProfile.STATE_CONNECTED) {
-            //GaTT接続成功しているので、サービスを検索する
-            gatt.discoverServices();
-            //サービス検索の成否はmBluetoothGattCallback.onServiceDiscoverdで受ける
-            }else if(newState==BluetoothProfile.STATE_DISCONNECTED){
-            mBluetoothGatt = null;
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                //GaTT接続成功しているので、サービスを検索する
+                gatt.discoverServices();
+                //サービス検索の成否はmBluetoothGattCallback.onServiceDiscoverdで受ける
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                mBluetoothGatt = null;
+            }
         }
-    }
 
-    @Override
-    //サービスの検出をする
-    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-        Log.d(TAG,"onServiceDiscoverd received" + status);
-        if(status == BluetoothGatt.GATT_SUCCESS){
-            BluetoothGattService service = gatt.getService(UUID.fromString(DEVICE_ACCELEROMATER_SERVICE_UUID));
+        @Override
+        //サービスの検出をする
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            Log.d(TAG, "onServiceDiscoverd received" + status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                BluetoothGattService service = gatt.getService(UUID.fromString(DEVICE_ACCELEROMATER_SERVICE_UUID));
+                //サービスを見つけたか判定
+                if (service == null) {
+                    setStatus(BleStatus.SERVICE_NOT_FOUND);
+                } else {
+                    setStatus(BleStatus.SERVICE_FOUND);
 
-    //サービスを見つけたか判定
-        if(service == null) {
-            setStatus(BleStatus.SERVICE_NOT_FOUND);
-        }else{
-            setStatus(BleStatus.SERVICE_FOUND);
+                    BluetoothGattCharacteristic characeristic = BluetoothGattCharacteristic(UUID.fromString(DEVICE_ACCELEROMATER_DATA_UUID));
 
-            BluetoothGattCharacteristic characeristic= BluetoothGattCharacteristic(UUID.fromString(DEVICE_ACCELEROMATER_DATA_UUID));
+                    //キャラクタリスティックを見つけたか判定
+                    if (characeristic == null) {
+                        setStatus(BleStatus.CHARACTERISTIC_NOT_FOUND);
+                    } else {
+                        //Notificationを要求する
+                        // ↓これ何だろ。あとて調べよっと
+                        boolean registered = gatt.setCharacteristicNotification(characeristic, true);
 
-        //キャラクタリスティックを見つけたか判定
-            if(characeristic == null) {
-                setStatus(BleStatus.CHARACTERISTIC_NOT_FOUND);
-            }else{
-                //Notificationを要求する
-                //↓これ何だろ。あとて調べよっと
-                boolean registered = gatt.setCharacteristicNotification(characeristic,true);
+                        //CharacteristicのNotification有効化
+                        BluetoothGattDescriptor descriptor = characeristic.getDescriptor(UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
+                        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                        gatt.writeDescriptor(descriptor);
+                        //↓通知設定が完了したかどうかチェック
+                        if (registered) {
+                            setStatus(BleStatus.NOTIFICATION_REGISTERED);
+                        } else {
+                            setStatus(BleStatus.NOTIFICATION_REGISTER_FAILED);
+                        }
+                    }
 
-                //CharacteristicのNotification有効化
-                BluetoothGattDescriptor descriptor = characeristic.getDescriptor(
-                        UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
-                descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
-                gatt.writeDescriptor(descriptor);
-                //↓通知設定が完了したかどうかチェック
-                if(registered) {
-                    setStatus(BleStatus.NOTIFICATION_REGISTERED);
-                }else{
-                    setStatus(BleStatus.NOTIGICATION_FAILED);
                 }
 
             }
 
+
         }
 
+        @Override
+        //Notificationを受信し状態を取得する
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            Log.d(TAG, "onCharacteristicRead:" + status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                System.out.println("READ成功");
+            }
+        }
+
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            Log.d(TAG, "onCharacteristicChanged");
+            //Characteristicの値更新通知
+
+            if (DEVICE_ACCELEROMATER_DATA_UUID.equals(characteristic.getUuid().toString())) {
+                //通知がある時の処理
+            }
+
+        }
+    };
+
+    private void setStatus(BleStatus status) {
+        mStatus = status; mHandler.sendMessage(status.message());
+    }
+    private enum BleStatus {
+        DISCONNECTED, SCANNING, SCAN_FAILED, DEVICE_FOUND, SERVICE_NOT_FOUND, SERVICE_FOUND,
+        CHARACTERISTIC_NOT_FOUND, NOTIFICATION_REGISTERED, NOTIFICATION_REGISTER_FAILED, CLOSED ;
+        public Message message() {
+            Message message = new Message();
+            message.obj = this;
+            return message;
         }
     }
-
-    @Override
-    //Notificationを受信し状態を取得する
-    public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic,int status){
-        Log.d(TAG,"onCharacteristicRead:"+status);
-        if (status == BluetoothGatt.GATT_SUCCESS){
-            System.out.println("READ成功");
-        }
-    }
-    public void onCharacteristicChanged(BluetoothGatt gatt,BluetoothGattCharacteristic characteristic) {
-        Log.d(TAG, "onCharacteristicChanged");
-        //Characteristicの値更新通知
-
-        if(DEVICE_ACCELEROMATER_DATA_UUID.equals(characteristic.getUuid().toString())){
-            //通知がある時の処理
-        }
-
-    }
-
-
-
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -235,4 +243,5 @@ BluetoothAdapter.LeScanCallback{
 
         return super.onOptionsItemSelected(item);
     }
+
 }

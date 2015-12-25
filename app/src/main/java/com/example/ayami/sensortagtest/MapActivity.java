493ae8619ed29d021155d4f0bd5ff92bae5b3344
karunngo <karu@ht.sfc.keio.ac.jp>
2015-12-25 14:37:46 +0900

    GPSパーミッションの追加

--- app/src/main/java/com/example/ayami/sensortagtest/MapActivity.java
+++ app/src/main/java/com/example/ayami/sensortagtest/MapActivity.java
@@ -1,5 +1,6 @@
 package com.example.ayami.sensortagtest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,6 +23,13 @@ public class MapActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        findViewById(R.id.addGPSButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("☆GPSボタンおしたよ！");
+
             }
         });
     }

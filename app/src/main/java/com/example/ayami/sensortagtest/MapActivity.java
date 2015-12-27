b8bd7c95778590554f8d5e285bee92245c2bf31c
karunngo <karu@ht.sfc.keio.ac.jp>
2015-12-27 16:45:38 +0900

    リスナーが一通り完成。ただしパーミッションが上手くいかない。これからマニフェストをいじる

--- app/src/main/java/com/example/ayami/sensortagtest/MapActivity.java
+++ app/src/main/java/com/example/ayami/sensortagtest/MapActivity.java
@@ -1,9 +1,11 @@
 package com.example.ayami.sensortagtest;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.view.View.OnClickListener;
 
 import android.location.Location;
@@ -17,9 +19,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
-public class MapActivity extends AppCompatActivity{
+public class MapActivity extends AppCompatActivity {
     LocationManager manager;
     LocationListener localistener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,23 +46,32 @@ public class MapActivity extends AppCompatActivity{
             }
         });
 
-
-        manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        localistener= new LocationListener(){
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        localistener = new LocationListener() {
             //onLocationChange:位置情報が変化した時に呼び出される
-            public void onLocationChanged(Location location){
+            public void onLocationChanged(Location location) {
                 Log.i("☆", "位置情報取得成功");
-                String latitude =Double.toString(location.getLatitude());
-                String longitude =Double.toString(location.getLongitude());
-                System.out.println("☆緯度"+latitude+"経度"+longitude);
+                String latitude = Double.toString(location.getLatitude());
+                String longitude = Double.toString(location.getLongitude());
+                System.out.println("☆緯度" + latitude + "経度" + longitude);
             }
 
-            public void onProviderEnabled(String procider){}
-            public void onProviderDisabled(String procider){}
-            public void onStatusChanged(String provider, int Status,Bundle extras){}
+            public void onProviderEnabled(String procider) {
+            }
+
+            public void onProviderDisabled(String procider) {
+            }
+
+            public void onStatusChanged(String provider, int Status, Bundle extras) {
+            }
         };
     }
 
-
-
+    @Override
+    //リスナーを登録
+    protected void onResume() {
+        super.onResume();
+        //Locationマネージャーにリスナーを設定
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 0, localistener);
+    }
 }
